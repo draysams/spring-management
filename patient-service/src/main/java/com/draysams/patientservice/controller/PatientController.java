ab7677c169f471372e263984e38501c28d680478
i343281 <samuel.davies@sap.com>
2025-07-11 10:20:46 +0100

    feat: add delete endpoint

--- patient-service/src/main/java/com/draysams/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/draysams/patientservice/controller/PatientController.java
@@ -10,6 +10,7 @@ import com.draysams.patientservice.service.PatientService;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,11 @@ public class PatientController {
 			@Validated({ Default.class }) @RequestBody PatientRequestDTO patientRequestDTO) {
 		PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
 		return ResponseEntity.ok().body(patientResponseDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+		patientService.deletePatient(id);
+		return ResponseEntity.noContent().build();
 	}
 }
